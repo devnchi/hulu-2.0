1fdb2e93146e36b43c45e4e326a9553b9d86076a
Chi Igbokwe <ch11gbokwedevelopment@gmail.com>
2022-02-08 18:46:32 -0500

    initial commit

---
+++ components/AppNav.js
@@ -0,0 +1,34 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon
+} from '@heroicons/react/outline';
+import Image from 'next/image';
+import AppNavItem from './AppNavItem';
+
+function AppNav() {
+  return (
+    <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+      <div className='flex flex-grow justify-evenly max-w-2xl'>
+        <AppNavItem title='HOME' Icon={HomeIcon} />
+        <AppNavItem title='TRENDING' Icon={LightningBoltIcon} />
+        <AppNavItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <AppNavItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <AppNavItem title='SEARCH' Icon={SearchIcon} />
+        <AppNavItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
+      <Image
+      className='object-contain'
+      src='https://links.papareact.com/ua6'
+      width={200}
+      height={100} 
+      />
+
+    </header>
+  )
+}
+
+export default AppNav;
